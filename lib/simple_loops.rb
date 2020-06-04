3b89fd21ce1d626ab4c42c02bbe8e1347913cb06
Eli Landau <eli.b.landau@gmail.com>
2020-06-04 18:53:11 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,4 +25,11 @@ def output_array(array)
   end
 end  
 
-
+def return_string_array(array)
+  count = 0 
+  while array[count] do
+    array[count].to_s
+    count += 1
+  end
+  puts array.to_s
+end  
