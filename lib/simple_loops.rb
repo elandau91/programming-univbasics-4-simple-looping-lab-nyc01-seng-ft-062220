1fd7dbfe4a5d647861f106aefee8a38b55a614ac
Eli Landau <eli.b.landau@gmail.com>
2020-06-04 18:48:12 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,11 +1,12 @@
 # Write your methods here
 
 def loop_message_five_times(message)
-  puts message
-  puts message
-  puts message
-  puts message
-  puts message
+ count = 0
+ while count < 5 do
+   puts message
+   count += 1
+ end
+ 
 end
 
 def loop_message_n_times(message, num)
