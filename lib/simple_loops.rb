bb75773aeb153d4b324384d7ed25e1ccdebb3e9b
Eli Landau <eli.b.landau@gmail.com>
2020-06-04 19:04:41 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -28,11 +28,12 @@ end
 def return_string_array(array)
   count = 0
   new_array = []
+  
   while array[count] do
   array[count].to_s
   new_array.push(array[count].to_s)
     count += 1
   end
-  puts new_array
+  puts new_array[]
 end  
   
